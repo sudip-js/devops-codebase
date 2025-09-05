fb1681b6fa9bb7546bf1914b413145101c74c68d
sudip-js <sudipbanerjee305@gmail.com>
2025-09-05 19:47:58 +0530

    add env file

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <h1>APP ID: {import.meta.env.VITE_APP_ID}</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
